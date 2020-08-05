d88e4ad42eedc370057c1e21966620d04edf7ce9
Martin Rejmon <mrejmon@gmail.com>
2020-08-05 22:26:03 +0200

    Add implementation and tests.

--- src/sage/combinat/words/morphism.py
+++ src/sage/combinat/words/morphism.py
@@ -91,6 +91,7 @@ Many other functionalities...::
 from __future__ import print_function
 
 from itertools import chain
+from itertools import islice
 
 from sage.misc.callable_dict import CallableDict
 from sage.structure.sage_object import SageObject
@@ -3164,3 +3165,81 @@ class WordMorphism(SageObject):
                 basis.extend((factor[0])(M).right_kernel().basis())
 
         return M._column_ambient_module().change_ring(QQ).subspace(basis)
+
+    @staticmethod
+    def _check_if_equal_and_add_new_tail_if_possible(w1, w2, tails_stack, tails_set):
+        r"""
+        TODO
+        """
+        if len(w1) == len(w2) and w1 == w2:
+            return False
+        elif len(w1) < len(w2) and w1.is_prefix(w2):
+            tail = w2[len(w1):]
+            if tail not in tails_set:
+                tails_set.add(tail)
+                tails_stack.append(tail)
+        elif len(w1) > len(w2) and w1.has_prefix(w2):
+            tail = w1[len(w2):]
+            if tail not in tails_set:
+                tails_set.add(tail)
+                tails_stack.append(tail)
+        return True
+
+    def is_injective(self):
+        r"""
+        TODO
+        
+        EXAMPLES::
+        
+            sage: WordMorphism('a->0,b->10,c->110,d->111').is_injective()
+            True
+            sage: WordMorphism('a->0,b->010,c->01,d->10').is_injective()
+            False
+            
+        TEST::
+        
+            sage: WordMorphism('a->10,b->00,c->11,d->110').is_injective()
+            True
+            sage: WordMorphism('a->0,b->0,c->1,d->1').is_injective()
+            False
+            sage: WordMorphism('a->1,b->011,c->01110,d->1110,e->10011').is_injective()
+            False
+            sage: WordMorphism('').is_injective()
+            True
+            sage: WordMorphism('a->').is_injective()
+            False
+            sage: WordMorphism('a->b').is_injective()
+            True
+            sage: WordMorphism('a->,b->').is_injective()
+            False
+            sage: WordMorphism('a->a,b->').is_injective()
+            False
+            sage: WordMorphism('a->,b->b').is_injective()
+            False
+        """
+        values = self._morph.values()
+        # Edge case.
+        if len(values) == 1 and next(iter(values)).is_empty(): return False
+        # Tail 't' is a word such that a = bt or b = at, where either both 'a' and 'b' are "code words",
+        # or 'a' is a code word and 'b' is another tail.
+        # Morphism is injective iff no tail is equal to a code word.
+        
+        # A stack is used for keeping track of tails we still have to check.
+        tails_stack = []
+        # A set is used to quickly check whether we saw this tail already.  
+        tails_set = set()
+        
+        # In the first part of the algorithm we check the case where both 'a' and 'b' are code words.
+        for i, v1 in enumerate(values):
+            for v2 in islice(values, i + 1, None):
+                    if not WordMorphism._check_if_equal_and_add_new_tail_if_possible(v1, v2, tails_stack, tails_set):
+                        return False
+        # In the second part of the algorithm we check the case where 'a' is a code word and 'b' is another tail.
+        while len(tails_stack) != 0:
+            tail = tails_stack.pop()
+            for v in values:
+                if not WordMorphism._check_if_equal_and_add_new_tail_if_possible(tail, v, tails_stack, tails_set):
+                    return False
+        # No tail was equal to a codeword, morphism is injective.
+        return True
+    
